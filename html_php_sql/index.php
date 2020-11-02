fb6e16275c5d50d21896cc8ec58e27470544933f
Chritar <38163709+Chritar@users.noreply.github.com>
2020-11-02 22:26:23 +0100

    Create index.php

---
+++ html_php_sql/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+<title>Christians Page</title>
+<?php 
+  require_once "bootstrap.php"; 
+?>
+</head>
+
+<body>
+<div class=container>
+  <h1>Welcome to Autos Database</h1>
+  <p>
+  <a href="login.php">continue</a>
+  </p>
+</div>
+  <!-- <script src="js/scripts.js"></script> -->
+</body>
+</html>
